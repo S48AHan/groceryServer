d25180de89f96399fbca1949c0022d0669c1c4c8
S48AHan <saberahmedn2@gmail.com>
2024-01-25 19:27:50 +0600

    login problem

--- index.js
+++ index.js
@@ -8,12 +8,43 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Simulated user data (replace with a database in a real application)
-const users = [{ email: "demo@example.com", password: "demopwd", roles: "admin" }];
+const users = [
+  {
+    username: "Admin",
+    email: "demo@example.com",
+    password: "demopwd",
+    roles: ["admin"],
+  },
+  {
+    username: "user1",
+    email: "user1@example.com",
+    password: "user1pwd",
+    roles: ["user"],
+  },
+  {
+    username: "user2",
+    email: "user2@example.com",
+    password: "user2pwd",
+    roles: ["user"],
+  },
+  {
+    username: "user3",
+    email: "user3@example.com",
+    password: "user3pwd",
+    roles: ["user"],
+  },
+];
 const secretKey = "yourSecretKey"; // Replace with a secure secret key for JWT
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+
+// Route for getting users
+app.get("/users", (req, res) => {
+  console.log(users);
+  res.json(users);
+});
 
 app.get("/", (req, res) => {
   res.send("Server Running Successfully peacefully");
@@ -21,15 +52,17 @@ app.get("/", (req, res) => {
 
 app.post("/register", (req, res) => {
   const regData = req.body;
-  console.log(regData);
+  console.log(JSON.stringify(regData, null, 2)); // The third argument (2) is for indentation (optional)
+
   // Save user data to the database (or perform registration logic)
   // ...
 
-  res.send(regData);
+  res.json(regData);
 });
 
 app.post("/login", (req, res) => {
   const { email, pwd } = req.body;
+  console.log(req);
   console.log("Received login request:", { email, pwd });
 
   // Find user in the simulated user data
@@ -37,9 +70,13 @@ app.post("/login", (req, res) => {
 
   if (user) {
     // Create a JWT token with user information
-    const token = jwt.sign({ email: user.email, roles: user.roles }, secretKey, {
-      expiresIn: "1h", // Token expiration time (adjust as needed)
-    });
+    const token = jwt.sign(
+      { email: user.email, roles: user.roles },
+      secretKey,
+      {
+        expiresIn: "10s", // Token expiration time (adjust as needed)
+      }
+    );
 
     // Return the JWT token and roles
     res.json({ accessToken: token, roles: user.roles });
